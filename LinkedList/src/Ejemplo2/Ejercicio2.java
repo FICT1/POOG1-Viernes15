232af15be06ed7011e595db5eef7475d99bb5f33
FICT1 <isaquitopro10@gmail.com>
2026-05-15 16:17:10 -0600

    feat: agregar ejercicio de nombres con addfirst y addlast

--- LinkedList/src/Ejemplo2/Ejercicio2.java
+++ LinkedList/src/Ejemplo2/Ejercicio2.java
@@ -1,4 +1,30 @@
 package Ejemplo2;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Ejercicio2 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        LinkedList<String> nombres = new LinkedList<>();
+
+        System.out.print("Ingrese un nombre: ");
+        nombres.add(entrada.nextLine());
+
+        System.out.print("Ingrese otro nombre: ");
+        nombres.add(entrada.nextLine());
+
+        System.out.print("Ingrese un nombre al inicio: ");
+        nombres.addFirst(entrada.nextLine());
+
+        System.out.print("Ingrese un nombre al final: ");
+        nombres.addLast(entrada.nextLine());
+
+        System.out.println("\nLista de nombres:");
+        System.out.println(nombres);
+
+    }
 }
