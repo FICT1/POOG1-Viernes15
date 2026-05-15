dac2a068c15a2088e4a5a99de65633a362eca4fa
FICT1 <isaquitopro10@gmail.com>
2026-05-15 16:33:09 -0600

    feat: agregar orden descendente de numeros

--- OrdenaciondeListas/src/Ordenacion3/Ejercicio3.java
+++ OrdenaciondeListas/src/Ordenacion3/Ejercicio3.java
@@ -1,4 +1,38 @@
 package Ordenacion3;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Ejercicio3 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        ArrayList<Integer> numeros = new ArrayList<>();
+
+        try {
+
+            for (int i = 0; i < 5; i++) {
+
+                System.out.print("Ingrese un numero: ");
+                int numero = entrada.nextInt();
+
+                numeros.add(numero);
+
+            }
+
+            Collections.sort(numeros, Collections.reverseOrder());
+
+            System.out.println("\nNumeros ordenados de mayor a menor:");
+            System.out.println(numeros);
+
+        } catch (Exception e) {
+
+            System.out.println("Error: ingrese solamente numeros enteros.");
+
+        }
+
+    }
 }
