bc31c8ecd6ee4c25419704a9a1a1229e7c16b2fa
FICT1 <isaquitopro10@gmail.com>
2026-05-15 16:15:46 -0600

    feat: agregar ejercicio de numeros con linkedlist

--- LinkedList/src/Ejemplo1/Ejercicio1.java
+++ LinkedList/src/Ejemplo1/Ejercicio1.java
@@ -1,4 +1,38 @@
 package Ejemplo1;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        LinkedList<Integer> numeros = new LinkedList<>();
+
+        try {
+
+            System.out.print("Cuantos numeros desea ingresar: ");
+            int cantidad = entrada.nextInt();
+
+            for (int i = 0; i < cantidad; i++) {
+
+                System.out.print("Ingrese un numero: ");
+                int numero = entrada.nextInt();
+
+                numeros.add(numero);
+
+            }
+
+            System.out.println("\nLista de numeros:");
+            System.out.println(numeros);
+
+        } catch (Exception e) {
+
+            System.out.println("Error: solo se permiten numeros enteros.");
+
+        }
+
+    }
 }
