901f31c9f0228dfd3d1728233872bf7f56bd24c4
Antonio2170 <avargasr@uamv.edu.ni>
2026-05-15 16:01:21 -0600

    feat: Primer ejemplo ArrayList

---
+++ Ejemplo_Viernes15may/src/Ejemplo1/Ejercicio1.java
@@ -0,0 +1,23 @@
+package Ejemplo1;
+
+import java.util.ArrayList;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> numeros = new ArrayList<>();
+
+        numeros.add(10);
+        numeros.add(20);
+        numeros.add(30);
+        numeros.add(40);
+        numeros.add(50);
+
+        System.out.println("Elementos del ArrayList:");
+
+        for (Integer numero : numeros) {
+            System.out.println(numero);
+        }
+    }
+}
