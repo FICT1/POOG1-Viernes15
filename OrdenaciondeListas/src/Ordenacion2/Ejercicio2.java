92619b1194b556811f70e673d71a0cb7b52536f7
FICT1 <isaquitopro10@gmail.com>
2026-05-15 16:32:36 -0600

    feat: agregar orden alfabetico de nombres

--- OrdenaciondeListas/src/Ordenacion2/Ejercicio2.java
+++ OrdenaciondeListas/src/Ordenacion2/Ejercicio2.java
@@ -1,4 +1,30 @@
 package Ordenacion2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Ejercicio2 {
+
+    public static void main(String[] args) {
+
+        Scanner entrada = new Scanner(System.in);
+
+        ArrayList<String> nombres = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+
+            System.out.print("Ingrese un nombre: ");
+            String nombre = entrada.nextLine();
+
+            nombres.add(nombre);
+
+        }
+
+        Collections.sort(nombres);
+
+        System.out.println("\nNombres ordenados alfabeticamente:");
+        System.out.println(nombres);
+
+    }
 }
